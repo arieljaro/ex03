d672ea61e636d35e96d7f83760d07ff2de4f0dff
reutlev <reutlev3@gmail.com>
2015-12-12 15:47:26 +0200

    added common.h defination

---
+++ ex03/common.h
@@ -0,0 +1,27 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+//--------Library Includes--------//
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <windows.h>
+#include <stdlib.h>
+#include <WinBase.h>
+#include <tchar.h>
+
+//--------Definitions--------//
+#define LOG_ERROR(msg, ...) fprintf(stderr, "[ERROR] (%s:%d) - " msg "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_WARN(msg, ...) fprintf(stderr, "[WARNING] (%s:%d) - " msg "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define LOG_INFO(msg, ...) fprintf(stderr, "[INFO] (%s:%d) - " msg "\r\n", __FILE__, __LINE__, ##__VA_ARGS__)
+
+typedef enum {
+	SUCCESS = 0,
+	WRONG_PARAMETERS,
+	MALLOC_FAILED,
+	THREAD_CREATION_FAILED,
+	THREAD_RUN_FAILED,
+	WAIT_FOR_MULTIPLE_OBJECT_FAILED
+} ErrorCode;
+
+#endif //COMMON_H
